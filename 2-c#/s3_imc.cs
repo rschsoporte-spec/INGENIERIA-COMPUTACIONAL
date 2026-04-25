974d8d8fa592dd1ddc78e2fd9e3f4bc3bf3f1b4e
ikaya <vanstore@gmail.com>
2026-04-25 11:18:49 -0500

    Agregado ejemplos semana 3

---
+++ 2-c#/s3_imc.cs
@@ -0,0 +1,9 @@
+﻿double peso;
+double talla;
+double imc;
+Console.WriteLine("Ingrese Peso KG :");
+peso = double.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese Peso Altura :");
+talla = double.Parse(Console.ReadLine());
+imc = peso / Math.Pow(talla,2);
+Console.WriteLine("El IMC es :" + imc);
---
+++ 2-c#/s3_promedio.cs
@@ -0,0 +1,7 @@
+﻿//Entrada
+double nota1=double.Parse(Console.ReadLine());
+double nota2=double.Parse(Console.ReadLine());
+//Proceso
+double promedio=(nota1+nota2)/2;
+//Salida
+Console.WriteLine(promedio);
